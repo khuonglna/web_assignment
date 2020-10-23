b8f935f9399c2303d4c35e28231a4a0e3369341e
Quyen Ho <quyenhooppa@Quyens-MBP.net.fpt>
2020-10-23 23:07:25 +0700

    edit bootstrap4

--- home.php
+++ home.php
@@ -2,43 +2,44 @@
 <html>
 
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   </head>
 
 
   <style>
     h1 {
       font-weight: bold;
-      font-size: 50px;
-      padding-bottom: 10px;
+      font-size: 40px;
+      padding: 20px;
     }
 
     p {
       font-size: 20px;
-    } 
+    }
   </style>
    
 
   <body>
   <div class="container-fluid">
-    <div class="row">
-      <h1 class="text-center">Welcome to our online English test </h1>
+    <div class="row justify-content-md-center">
+      <h1>Welcome to our online English test </h1>
     </div>
 
-    <div class="row"> 
-      <div class="col-sm-5 col-md-offset-1"> 
-        <img class="img-fluid img-rounded" src="./images/test_img.jpg" width="100%" height="300">
+    <div class="row justify-content-md-center"> 
+      <div class="col col-lg-5"> 
+        <img class="rounded shadow-lg" src="./images/test_img.jpg" width="100%" height="300">
       </div>
 
-      <div class="text-justify col-sm-5">
+      <div class="text-justify col col-lg-5">
         <p> 
           We are here to help you learn and improve your English.<br>
           We have lots of exam with different topics and levels for you to test your English.<br>
           Click "Test" to do the exam now! 
-        <p>
+        </p>
       </div>
     </div>
   </div>
