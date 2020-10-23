9b6ebf0ba7ab98c99f32f761d2e12844332726b3
Quyen Ho <quyenhooppa@Quyens-MBP.net.fpt>
2020-10-23 22:24:28 +0700

    use bootstrap

--- home.php
+++ home.php
@@ -1,3 +1,46 @@
-<?php
-  echo "This is home page!";
-?>
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </head>
+
+
+  <style>
+    h1 {
+      font-weight: bold;
+      font-size: 50px;
+      padding-bottom: 10px;
+    }
+
+    p {
+      font-size: 20px;
+    } 
+  </style>
+   
+
+  <body>
+  <div class="container-fluid">
+    <div class="row">
+      <h1 class="text-center">Welcome to our online English test </h1>
+    </div>
+
+    <div class="row"> 
+      <div class="col-sm-5 col-md-offset-1"> 
+        <img class="img-fluid img-rounded" src="./images/test_img.jpg" width="100%" height="300">
+      </div>
+
+      <div class="text-justify col-sm-5">
+        <p> 
+          We are here to help you learn and improve your English.<br>
+          We have lots of exam with different topics and levels for you to test your English.<br>
+          Click "Test" to do the exam now! 
+        <p>
+      </div>
+    </div>
+  </div>
+  </body>
+</html>
