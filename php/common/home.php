bc276b75e61919dba4e7a8dc93a6c638866ad1eb
Quyen Ho <quyenhooppa@Quyens-MBP.net.fpt>
2020-10-27 18:06:57 +0700

    add slideshow

--- php/common/home.php
+++ php/common/home.php
@@ -9,26 +9,57 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
-
-<style>
-  h2 {
-    font-weight: bold;
-  }
-
-  p {
-    font-size: 20px;
-  }
+<style> 
+  .my-sidebar {
+      -ms-flex: 0 0 500px;
+      flex: 0 0 500px;
+  } 
 </style>
 
-
 <body>
-  <div class="container-fluid">
+  <div class="container">
     <div class="row justify-content-md-center">
-      <div class="col col-lg-4">
-        <img class="rounded shadow-lg" src="./images/test_img.jpg" width="100%" height="300">
+      <div id="demo" class="text-center col my-sidebar carousel slide" data-ride="carousel">
+        <ul class="carousel-indicators">
+          <li data-target="#demo" data-slide-to="0" class="active"></li>
+          <li data-target="#demo" data-slide-to="1"></li>
+          <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
+
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block rounded shadow-lg" src="http://mediad.publicbroadcasting.net/p/michigan/files/styles/medium/public/201402/standardized_test.jpg" width="500" height="250">
+            <div class="carousel-caption">
+              <h3>Los Angeles</h3>
+              <p>We had such a great time in LA!</p>
+            </div>   
+          </div>
+          <div class="carousel-item">
+            <img class="d-block rounded shadow-lg" src="http://www.minddisorders.com/photos/wide-range-achievement-test-973.jpg" width="500" height="250">
+            <div class="carousel-caption">
+              <h3>Chicago</h3>
+              <p>Thank you, Chicago!</p>
+            </div>   
+          </div>
+          <div class="carousel-item">
+            <img class="d-block rounded shadow-lg" src="https://3.bp.blogspot.com/-0oROeMnz0AQ/UxsMz5PjMHI/AAAAAAAACHs/3Nm10eh8f1Y/s1600/quiz+night.gif" width="500" height="250">
+            <div class="carousel-caption">
+              <h3>New York</h3>
+              <p>We love the Big Apple!</p>
+            </div>   
+          </div>
+        </div>
+  
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </a>
+
       </div>
 
-      <div class="text-justify col col-lg-4">
+      <div class="text-justify col my-sidebar">
         <div class="row">
           <h2>Welcome to our online English test </h2>
         </div>
