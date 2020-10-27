b531d257d138186ea2abfec35a00499d341b4dea
Quyen Ho <quyenhooppa@Quyens-MBP.net.fpt>
2020-10-27 18:23:20 +0700

    edit caption

--- php/common/home.php
+++ php/common/home.php
@@ -11,48 +11,48 @@
 
 <style> 
   .my-sidebar {
-      -ms-flex: 0 0 500px;
-      flex: 0 0 500px;
+      -ms-flex: 0 0 550px;
+      flex: 0 0 550px;
   } 
 </style>
 
   <div class="container">
     <div class="row justify-content-md-center">
-      <div id="demo" class="text-center col my-sidebar carousel slide" data-ride="carousel">
+      <div id="slideshow" class="text-center col my-sidebar carousel slide" data-ride="carousel">
         <ul class="carousel-indicators">
-          <li data-target="#demo" data-slide-to="0" class="active"></li>
-          <li data-target="#demo" data-slide-to="1"></li>
-          <li data-target="#demo" data-slide-to="2"></li>
+          <li data-target="#slideshow" data-slide-to="0" class="active"></li>
+          <li data-target="#slideshow" data-slide-to="1"></li>
+          <li data-target="#slideshow" data-slide-to="2"></li>
         </ul>
 
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img class="d-block rounded shadow-lg" src="http://mediad.publicbroadcasting.net/p/michigan/files/styles/medium/public/201402/standardized_test.jpg" width="500" height="250">
+            <img class="d-block rounded shadow-lg" src="http://mediad.publicbroadcasting.net/p/michigan/files/styles/medium/public/201402/standardized_test.jpg" width="550" height="300">
             <div class="carousel-caption">
-              <h3>Los Angeles</h3>
-              <p>We had such a great time in LA!</p>
+              <h3>Image 1</h3>
+              <p>Something</p>
             </div>   
           </div>
           <div class="carousel-item">
-            <img class="d-block rounded shadow-lg" src="http://www.minddisorders.com/photos/wide-range-achievement-test-973.jpg" width="500" height="250">
+            <img class="d-block rounded shadow-lg" src="http://www.minddisorders.com/photos/wide-range-achievement-test-973.jpg" width="550" height="300">
             <div class="carousel-caption">
-              <h3>Chicago</h3>
-              <p>Thank you, Chicago!</p>
+              <h3>Image 2</h3>
+              <p>Something</p>
             </div>   
           </div>
           <div class="carousel-item">
-            <img class="d-block rounded shadow-lg" src="https://3.bp.blogspot.com/-0oROeMnz0AQ/UxsMz5PjMHI/AAAAAAAACHs/3Nm10eh8f1Y/s1600/quiz+night.gif" width="500" height="250">
+            <img class="d-block rounded shadow-lg" src="https://3.bp.blogspot.com/-0oROeMnz0AQ/UxsMz5PjMHI/AAAAAAAACHs/3Nm10eh8f1Y/s1600/quiz+night.gif" width="550" height="300">
             <div class="carousel-caption">
-              <h3>New York</h3>
-              <p>We love the Big Apple!</p>
+              <h3>Image 3</h3>
+              <p>Something</p>
             </div>   
           </div>
         </div>
   
-        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <a class="carousel-control-prev" href="#slideshow" data-slide="prev">
           <span class="carousel-control-prev-icon"></span>
         </a>
-        <a class="carousel-control-next" href="#demo" data-slide="next">
+        <a class="carousel-control-next" href="#slideshow" data-slide="next">
           <span class="carousel-control-next-icon"></span>
         </a>
       </div>
@@ -71,27 +71,3 @@
       </div>
     </div>
   </div>
-
-  
-<!-- 
-<div class="container" style="margin-top:30px">
-</div>
-
-      <div class="text-justify col my-sidebar">
-        <div class="row">
-          <h2>Welcome to our online English test </h2>
-        </div>
-        <div class="row">
-          <p>
-            We are here to help you learn and improve your English.<br>
-            We have lots of exam with different topics and levels for you to test your English.<br>
-            Click "Test" to do the exam now!
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div> -->
-<!-- </body>
-
-</html> -->
