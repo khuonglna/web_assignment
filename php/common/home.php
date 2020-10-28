f2b60d2c88f9cc3725179e5b9c7316cbbb760be5
Quyen Ho <quyenhooppa@Quyens-MBP.net.fpt>
2020-10-28 22:41:23 +0700

    edit slideshow

--- php/common/home.php
+++ php/common/home.php
@@ -9,16 +9,8 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head> -->
 
-<style> 
-  .my-sidebar {
-      -ms-flex: 0 0 550px;
-      flex: 0 0 550px;
-  } 
-</style>
-
   <div class="container">
-    <div class="row justify-content-md-center">
-      <div id="slideshow" class="text-center col my-sidebar carousel slide" data-ride="carousel">
+      <div id="slideshow" class="carousel slide" data-ride="carousel">
         <ul class="carousel-indicators">
           <li data-target="#slideshow" data-slide-to="0" class="active"></li>
           <li data-target="#slideshow" data-slide-to="1"></li>
@@ -27,47 +19,52 @@
 
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img class="d-block rounded shadow-lg" src="http://mediad.publicbroadcasting.net/p/michigan/files/styles/medium/public/201402/standardized_test.jpg" width="550" height="300">
-            <div class="carousel-caption">
-              <h3>Image 1</h3>
-              <p>Something</p>
-            </div>   
+            <div class="row justify-content-md-center">
+              <div class="col text-center home-image-sidebar">
+                <img class="d-block rounded shadow-lg" src="http://mediad.publicbroadcasting.net/p/michigan/files/styles/medium/public/201402/standardized_test.jpg" width="700" height="300">
+              </div>
+
+              <div class="col home-text-sidebar"> 
+                <h2>Welcome to our online English test.</h2>
+              </div>  
+            </div>
           </div>
+
           <div class="carousel-item">
-            <img class="d-block rounded shadow-lg" src="http://www.minddisorders.com/photos/wide-range-achievement-test-973.jpg" width="550" height="300">
-            <div class="carousel-caption">
-              <h3>Image 2</h3>
-              <p>Something</p>
-            </div>   
+            <div class="row justify-content-md-center">
+              <div class="col text-center home-image-sidebar">
+                <img class="d-block rounded shadow-lg" src="http://www.minddisorders.com/photos/wide-range-achievement-test-973.jpg" width="700" height="300">
+              </div>
+
+              <div class="col text-justify home-text-sidebar"> 
+                <h5>We are here to help you learn and improve your English.</h5>
+              </div>  
+            </div>
           </div>
+
           <div class="carousel-item">
-            <img class="d-block rounded shadow-lg" src="https://3.bp.blogspot.com/-0oROeMnz0AQ/UxsMz5PjMHI/AAAAAAAACHs/3Nm10eh8f1Y/s1600/quiz+night.gif" width="550" height="300">
-            <div class="carousel-caption">
-              <h3>Image 3</h3>
-              <p>Something</p>
-            </div>   
+            <div class="row justify-content-md-center">
+              <div class="col text-center home-image-sidebar">
+                <img class="d-block rounded shadow-lg" src="http://mediad.publicbroadcasting.net/p/michigan/files/styles/medium/public/201402/standardized_test.jpg" width="700" height="300">
+              </div>
+
+              <div class="col text-justify home-text-sidebar"> 
+                <h5>We have lots of exams with different topics and levels for you to test your English.</h5>
+              </div>  
+            </div>
           </div>
-        </div>
+          
   
         <a class="carousel-control-prev" href="#slideshow" data-slide="prev">
-          <span class="carousel-control-prev-icon"></span>
+          <!-- <span class="carousel-control-prev-icon"></span> -->
+          <!-- <i style="color:#000; font-size:30px;" class="fas fa-arrow-alt-circle-left"></i>
+          <span class="sr-only">Previous</span> -->
         </a>
         <a class="carousel-control-next" href="#slideshow" data-slide="next">
-          <span class="carousel-control-next-icon"></span>
+          <!-- <span class="carousel-control-next-icon"></span> -->
+          <!-- <i style="color:#000; font-size:30px" class="fas fa-arrow-alt-circle-right"></i>
+          <span class="sr-only">Next</span> -->
         </a>
-      </div>
-
-      <div class="text-justify col my-sidebar">
-        <div class="row">
-            <h2>Welcome to our online English test </h2>
-          </div>
-        <div class="row">
-          <p>
-            We are here to help you learn and improve your English.<br>
-            We have lots of exam with different topics and levels for you to test your English.<br>
-            Click "Test" to do the exam now!
-          </p>
-          </div>
       </div>
     </div>
   </div>
