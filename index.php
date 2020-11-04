a1a84e44df40bc7a72f21127879ce89f81eed984
chrislenguyen <34916267+chrislenguyen@users.noreply.github.com>
2020-11-05 00:32:39 +0700

    add pushing data to frontend

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     if (isset($_GET['page'])) {
         $page = $_GET['page'];
         if ($page == "login") {
-            include "php/common/login/$page.php";
+            include "views/exam_view.php";
         } 
         elseif ($page == "sign_up") {
             $controller = isset($_GET['controller'])? $_GET['controller'].'Controller' : 'UserController' ;
@@ -18,8 +18,8 @@
         elseif ($page == "home") {
             include "views/$page.php";
         } 
-        elseif ($page == "test") {
-            include "php/component/test/$page.php";
+        elseif ($page == "exam_view") {
+            include "view/$page.php";
         } 
         else include "$page.php";
     } 
--- models/question.php
+++ models/question.php
@@ -1,6 +1,7 @@
 <?php
 require_once('db_model.php');
 require_once('exam.php');
+require_once('answer.php');
 
 class Question
 {
@@ -30,20 +31,47 @@ class QuestionModel extends DbModel
         $questionList = array();
         $conn = $this->connect();
         $sql = "SELECT 
-                        *
-                    FROM 
-                        QUESTION
-                    WHERE 
-                        _LEVEL = $ category AND Q_CATEGORY = $level";
+                    Q_ID,
+                    Q_TEXT
+                FROM 
+                    QUESTION
+                WHERE 
+                    Q_LEVEL = $category AND Q_CATEGORY = $level";
         $res = mysqli_query($conn, $sql);
         if (mysqli_num_rows($res) > 0) {
-            while ($row = mysqli_fetch_row($res)) {
+            while ($row = mysqli_fetch_assoc($res)) {
                 $question = new Question();
                 $answerModel = new AnswerModel();
-                $question->setAnswerList($answerModel->queryListAnswerByQuestionId($row["q_id"]));
+                $temp = $row["Q_ID"];
+                $temp2 = $answerModel->queryListAnswerByQuestionId($temp);
+                $question->setAnswerList($temp2);
                 $questionList[] = $question;
             }
         }
         return $questionList;
     }
+
+    function test ($category, $level) {
+        $questionList = array();
+        $conn = $this->connect();
+        $sql = "SELECT
+                    q.q_id,
+                    q.q_text,
+                    a.a_id,
+                    a.a_text,
+                    a.a_correct_flag
+                FROM
+                    question q
+                INNER JOIN answer a ON
+                    a.q_id = q.q_id
+                WHERE
+                    q.q_level = $level AND q.q_category = $category";
+        $data =array();
+        $res = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_assoc($res)) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+
 }
