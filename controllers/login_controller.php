352eaf6d652458dc3410ab8862a4faa4aa37330a
chrislenguyen <34916267+chrislenguyen@users.noreply.github.com>
2020-11-05 12:49:36 +0700

    add login

---
+++ controllers/login_controller.php
@@ -0,0 +1,34 @@
+<?php 
+	require_once('models/user_model.php');
+    class UserController
+	{
+        private function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+		public function getUser() {
+			$username = isset($_POST['name'])? $_POST['name']: '' ;
+			$password = md5(isset($_POST['pass'])? $_POST['pass']: '');
+			if ($password != '' && $username != '' ) {
+				$usermodel = new UserModel();
+                $user = $usermodel->login($username , $password);
+                if ($user) {
+					require_once('views/login_view.php');
+					$_SESSION["role"] = mysqli_fetch_assoc($user)["role"];
+					echo $_SESSION["role"];
+                } else {
+				 	require_once('views/login_view.php');
+                }
+			} else {
+				require_once('views/login_view.php');
+			}
+		}
+	} 
+?>
+
+<script>
+    
+</script>
