199d7a315240c57d9511fb90468e610621a8699f
Quyen Ho <quyenhooppa@Quyens-MBP.net.fpt>
2020-11-09 13:35:05 +0700

    return add result

--- controllers/test.php
+++ controllers/test.php
@@ -1,10 +1,19 @@
 <?php
-        // require_once __DIR__ . '/../controllers/addexam_controller.php';
-        include 'addexam_controller.php';
-        $res = $_REQUEST["function"];
+        $res = $_REQUEST['function'];
         if ($res == "addQuestion") {
+                $question = $_REQUEST['question'];
+                $category = $_REQUEST['category'];
+                $level = $_REQUEST['level'];
+                $correct = $_REQUEST['correct'];
+
+                $ansList = array();
+                $ansList[0] = $_REQUEST['answer1'];
+                $ansList[1] = $_REQUEST['answer2'];
+                $ansList[2] = $_REQUEST['answer3'];
+
+                include 'addexam_controller.php';
                 $examController = new ExamController();
-                $result = $examController->addQuestion();
+                $result = $examController->addQuestion ($question, $category, $level, $ansList, $correct);
                 echo json_encode($result);
         } else {
                 echo "guest";
