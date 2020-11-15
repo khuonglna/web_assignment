f175dab3c0e0f12a5d7c4848d948caba3d02a636
chrislenguyen <34916267+chrislenguyen@users.noreply.github.com>
2020-11-15 21:56:52 +0700

    mod ignore category

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
             $usercontroller = new $controller();
             $usercontroller-> $action();
         } elseif ($page == "exam_view") {
-            include "views/category_view.php";
+            include "views/exam_view.php";
         } else {
             include "views/$page.php";
         }
