2d41f021ef9bc6d8142b0733b87aed0ded1c3a74
chrislenguyen <34916267+chrislenguyen@users.noreply.github.com>
2020-11-18 00:40:57 +0700

    add doing exam func

--- views/js/exam_view.js
+++ views/js/exam_view.js
@@ -1,59 +1,152 @@
 var QUESTION_NUMBER = 10;
 var ANSWER_NUMBER = 3;
+var METHOD = "POST";
+var ASYN = true;
 function showExam(questionList) {
 	var exam = document.getElementById("examForm");
 
 	for (i = 0; i < QUESTION_NUMBER; i++) {
-        // document.getElementById("submitBtn")
-        var pbreak = document.createElement("br");
-        exam.appendChild(pbreak);
-		console.log(questionList[i].ans1);
+		// document.getElementById("submitBtn")
+		var pbreak = document.createElement("br");
+		exam.appendChild(pbreak);
+		var hidden = document.createElement("input");
 		var questionElement = document.createElement("p");
 		var questionText = document.createTextNode(
 			i + 1 + "." + questionList[i].q_text
 		);
-		questionElement.id = questionList[i].q_id;
-		questionElement.setAttribute("name", "question" + (i + 1));
+		hidden.id = "hidden_q" + i;
+		hidden.setAttribute("type", "hidden");
+		hidden.setAttribute("value", questionList[i].q_id);
+		// questionElement.setAttribute("name", "q" + i);
 		questionElement.appendChild(questionText);
+		exam.appendChild(hidden);
 		exam.appendChild(questionElement);
 		for (j = 0; j < ANSWER_NUMBER; j++) {
 			var answerInput = document.createElement("input");
 			var answerLabel = document.createElement("label");
 			var answerLabelText = document.createTextNode(
 				questionList[i].ans[j].a_text
-            );
-            answerInput.id = questionList[i].ans[j].a_id;
+			);
+			// answerLabel.setAttribute("style", "color: red");
+			answerLabel.id = questionList[i].ans[j].a_id;
 			answerInput.setAttribute("class", "form-check-input");
 			answerInput.setAttribute("type", "radio");
-			answerInput.setAttribute("name", "ans" + (i + 1));
+			answerInput.setAttribute("name", "a" + i);
 			answerInput.setAttribute("value", questionList[i].ans[j].a_id);
 			answerLabel.appendChild(answerLabelText);
 			exam.appendChild(answerInput);
-            exam.appendChild(answerLabel);
-            var pbreak = document.createElement("br");
-            exam.appendChild(pbreak);
+			exam.appendChild(answerLabel);
+			var pbreak = document.createElement("br");
+			exam.appendChild(pbreak);
 		}
 	}
 }
 
-// function getExam() {
-var ajax = new XMLHttpRequest();
-var method = "POST";
-var url = "controllers/question_ptj.php";
-var asynchronous = true;
+function getExamForm() {
+	var ajax = new XMLHttpRequest();
+	var url = "controllers/question_ptj.php?do=getExam";
+	ajax.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			var data = this.responseText;
+			// console.log(this.responseText);
+			questionList = JSON.parse(data);
+			showExam(questionList);
+		}
+	};
+	ajax.open(METHOD, url, ASYN);
+	ajax.send();
+}
 
-ajax.onreadystatechange = function () {
-	if (this.readyState == 4 && this.status == 200) {
-		var data = this.responseText;
-		console.log(data);
-		questionList = JSON.parse(data);
-		showExam(questionList);
+function checkAllRadio() {
+	var counter = 0;
+	var inputList = document.getElementsByClassName("form-check-input");
+	for (i of inputList) {
+		if (i.checked) {
+			counter++;
+		}
 	}
-};
-ajax.open(method, url, asynchronous);
-ajax.send();
-// }
+	// console.log(counter);
+	return counter;
+}
+
+function getSubmission() {
+	var data = "";
+	var answerList = new Array();
+	var answer = document.getElementsByClassName("form-check-input");
+	for (i of answer) {
+		if (i.checked) {
+			answerList.push(i.value);
+		} else {
+			i.disabled = true;
+		}
+	}
+	// console.log(answerList);
+
+	for (i = 0; i < QUESTION_NUMBER; i++) {
+		data +=
+			"&q" +
+			i +
+			"=" +
+			document.getElementById("hidden_q" + i).value +
+			"&a" +
+			i +
+			"=" +
+			answerList[i];
+	}
+
+	submitBtn = document.getElementById("submitBtn");
+	submitBtn.textContent = "Retry";
+	submitBtn.setAttribute("onclick", "retry()");
+
+	// console.log(data);
+	return data;
+}
+
+function showResult(result) {
+	for (i of result.green) {
+		document.getElementById(i).setAttribute("style", "color: green");
+	}
+	for (i of result.red) {
+		document.getElementById(i).setAttribute("style", "color: red");
+	}
+	document.getElementById("score").appendChild(document.createTextNode(result.score + "/100"));
+}
+
+function retry() {
+	console.log("RETRY");
+	location.reload();
+}
 
 function submitForm() {
-    document.getE
+	var ajax = new XMLHttpRequest();
+	var checkSelecting = checkAllRadio();
+	var decision;
+	var url = "controllers/question_ptj.php?do=submitExam";
+	if (checkSelecting < QUESTION_NUMBER) {
+		alert(
+			"YOU STILL HAVE " +
+				(QUESTION_NUMBER - checkSelecting) +
+				" QUESTION LEFT"
+		);
+	} else {
+		decision = confirm("ARE YOU SURE?");
+	}
+	if (decision) {
+		var submissionData = getSubmission();
+		var data;
+		var result;
+		// console.log(submissionData);
+		ajax.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				data = this.responseText;
+				result = JSON.parse(data);
+				// console.log(data);
+				console.log(result);
+				console.log(result.score);
+				showResult(result);
+			}
+		};
+		ajax.open(METHOD, url + submissionData, ASYN);
+		ajax.send();
+	}
 }
