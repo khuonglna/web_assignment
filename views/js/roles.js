5d359432751fd507cb4e1103f9b6c25ce71bd34d
chrislenguyen <34916267+chrislenguyen@users.noreply.github.com>
2020-12-13 17:32:46 +0700

    clean console

--- views/js/roles.js
+++ views/js/roles.js
@@ -12,7 +12,7 @@ ajax.send();
 
 ajax.onreadystatechange = function () {
 	if (this.readyState == 4 && this.status == 200) {
-		console.log(this.responseText);
+		// console.log(this.responseText);
 		var data = JSON.parse(this.responseText);
 		if (!data.username) {
 			state = LOGOUT;
